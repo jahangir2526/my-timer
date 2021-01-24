5872e30841be12d24f5846f747f55a97e87938e4
Jahangir Alam <jahangir2526@gmail.com>
2021-01-24 10:41:24 -0500

    v1 and v2 added

---
+++ v1/timer.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# my-timer v1
+while :
+do
+echo -n "v1: "
+date
+sleep 1
+done
---
+++ v2/timer.sh
@@ -0,0 +1,9 @@
+cat > timer.sh
+#!/bin/sh
+# my-timer v2
+while :
+do
+echo -n "v2: "
+date
+sleep 1
+done
